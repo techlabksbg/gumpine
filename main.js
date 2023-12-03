c0302efd980c9490cbcf76fc8d8e0954385f1c5f
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2023-12-03 15:53:56 +0100

    puzzle generation working (it seems)

--- main.js
+++ main.js
@@ -3,10 +3,17 @@ import {Moving} from "./moving.mjs"
 import {generator} from "./generator.mjs"
 
 
-for (let hasen of generator.hasenPlaetze(2)) {
-    for (let pilze of generator.pilzPlaetze(2, hasen)) {
+for (let hasen of generator.hasenPlaetze(3)) {
+    for (let pilze of generator.pilzPlaetze(3, hasen)) {
         let f = new Fix(pilze);
         let m = new Moving(f, hasen, []);
-        console.log(m.toString());
+        let sol = m.explore();
+        if ('positions' in sol && sol.solution.length>17) {
+            console.log(`New puzzle! pathlength=${sol.solution.length}, positions=${sol.positions}`);
+            for (let s of sol.solution) {
+                let schritt = m.fromNumber(s);
+                console.log(schritt.toString());
+            }
+        }
     }
 }
