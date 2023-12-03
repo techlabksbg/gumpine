5848f899550c54c69288e037a39ea8a74ae7ce9d
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2023-12-03 15:42:46 +0100

    pilzPlaetze tut

--- generator.mjs
+++ generator.mjs
@@ -1,3 +1,5 @@
+import { util } from "./util.mjs"
+
 
 export const generator = {
     "hasenPlaetze" : function(anzahlHasen) {
@@ -15,8 +17,46 @@ export const generator = {
         }
     },
 
-    "pilzPlaetze" : function(anzahlPilze, hasen) {
+    "pilzPlaetze" : function(anzahlPilze, hasen, size=5) {
+        let frei = new Array(size).fill(0).map(e=>new Array(size).fill(true));
+        for (let hase of hasen) frei[hase[0]][hase[1]] = false;
 
+
+        let nextSquare = function(p) {
+            p = [...p];
+            while (true) {
+                p[0]++;
+                if (p[0]>=size) {
+                    p[0]=0;
+                    p[1]++;
+                }
+                if (p[1]>=size) {
+                    return false;
+                }
+                if (frei[p[0]][p[1]]) {
+                    return p;
+                }
+            }
+        }
+
+        let pilzPositionen = [];
+
+        let platzieren = function(pilze, wievielnoch, abwo) {
+            if (wievielnoch==0) {
+                pilzPositionen.push(util.deepcopy(pilze));
+                return;
+            }
+            let p = nextSquare(abwo);
+            while (p!==false) {
+                pilze.push(p);
+                platzieren(pilze, wievielnoch-1, p);
+                pilze.pop();
+                p = nextSquare(p);
+            }
+        }
+        // Array pilzPositionen befüllen
+        platzieren([], anzahlPilze, [-1,0]);
+        return pilzPositionen;
     }
 
 }
--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 import {Fix} from "./fix.mjs"
 import {Moving} from "./moving.mjs"
+import {generator} from "./generator.mjs"
 
-let f = new Fix([[1,0], [2,2], [3,0]]);
-let m = new Moving(f, [[0,0], [4,4]],[]);
-    /*[{'pos':3, 'start':[3,0], 'vec':[0,1]},
-     {'pos':0, 'start':[0,3], 'vec':[1,0]}]); */
 
-console.log(m.toString());
-console.log(m.explore());
+for (let hasen of generator.hasenPlaetze(2)) {
+    for (let pilze of generator.pilzPlaetze(2, hasen)) {
+        let f = new Fix(pilze);
+        let m = new Moving(f, hasen, []);
+        console.log(m.toString());
+    }
+}
