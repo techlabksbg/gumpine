335759d3005ce4caafbd3bd515b7459e8115388a
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2023-12-14 15:02:10 +0100

    kick events in touch and mouse

--- ui.js
+++ ui.js
@@ -13,6 +13,66 @@ window.addEventListener('load', function(){
         draw(); 
     }
 
+    let moving = false;
+    let startMove = [0,0];
+    let endMove = [0,0];
+
+    function processMove() {
+        console.log(`Moving from ${startMove} to ${endMove}`);
+    }
+
+    function getElementCoordinates(e, touchMove=false) {
+        if (!touchMove) {
+            var rect = e.target.getBoundingClientRect();
+            var x = e.clientX - rect.left; //x position within the element.
+            var y = e.clientY - rect.top;  //y position within the element.
+            return [x,y];
+        } else {
+            var xPos = e.targetTouches[0].pageX;
+            var yPos = e.targetTouches[0].pageY;
+            var rect = e.target.getBoundingClientRect();
+            var x = xPos - rect.left; //x position within the element.
+            var y = yPos - rect.top;  //y position within the element.
+            return [x,y];
+        }
+    }
+
+    function startInteraction(e, touchMove=false) {
+        e.preventDefault();            
+        let [x,y] = getElementCoordinates(e, touchMove);
+        moving = true;
+        startMove = [x,y];
+    }
+    function dragInteraction(e, touchMove=false) {
+        if (!touchMove) {
+            if (e.buttons==0) return;
+        }
+        e.preventDefault();
+        let [x,y] = getElementCoordinates(e, touchMove);
+        endMove = [x,y];
+    }
+    function stopInteraction(e, touchMove) {
+        e.preventDefault();
+        if (!touchMove) {
+            let [x,y] = getElementCoordinates(e, touchMove);
+            endMove=[x,y];
+        }
+        moving = false;
+        processMove();
+    }
+
+    function initEventHandlers(el) {
+        el.addEventListener("touchstart", (e)=>startInteraction(e, true), false);
+        el.addEventListener("mousedown", (e)=>startInteraction(e, false), false);
+        el.addEventListener("touchmove", (e)=>dragInteraction(e, true), false);
+        el.addEventListener("mousemove", (e)=>dragInteraction(e, false), false);
+        el.addEventListener("touchend", (e)=>stopInteraction(e, true), false);
+        el.addEventListener("touchcancel", (e)=>stopInteraction(e, true), false);
+        el.addEventListener("touchleave", (e)=>stopInteraction(e, true), false);
+        el.addEventListener("mouseup", (e)=>stopInteraction(e, false), false);
+
+    }
+    initEventHandlers(canvas);
 
     function draw() {
         let { w, h } = canvas.getBoundingClientRect();
